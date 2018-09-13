8114b4345b17208dc72f27603c741e8a6d9088cb
Szlavicsek <semper576@gmail.com>
2018-09-13 20:13:51 +0200

    login derp

--- src/js/modules/Login.js
+++ src/js/modules/Login.js
@@ -1,28 +1,12 @@
 const Login = (() => {
+
   const $signInText = document.querySelector('.sign-in-text')
   const $avatarImage = document.querySelector('.avatar-image')
   const $googleSignInButton = document.querySelector('.sign-in-with-google-button')
   const $loginCube = document.querySelector('.login-cube')
 
-  const onSuccess = (googleUser) => {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-  }
-
-  const onFailure = (error) => {
-    console.log(error);
-  }
-
-  const renderButton = () => {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
-  }
-
   const eventListeners = () => {
     $signInText.addEventListener("click", function() {
-      console.log("hello");
       $loginCube.classList.toggle("login-cube-turned")
     })
   }
@@ -30,12 +14,11 @@ const Login = (() => {
   const init = () => {
     eventListeners()
   }
+
   return {
-    onSuccess,
-    onFailure,
-    renderButton,
     init
   }
+
 })()
 
 export default Login
