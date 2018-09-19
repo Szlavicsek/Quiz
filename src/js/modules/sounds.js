cef9cc3fb7fba768a698cf2b1447b93cd1f6c958
Szlavicsek <semper576@gmail.com>
2018-09-19 11:07:26 +0200

    tzt

--- src/js/modules/sounds.js
+++ src/js/modules/sounds.js
@@ -8,14 +8,14 @@ Howler.volume(0.5);
 export const sounds = {
   star: new Howl({
     src: ['./src/assets/sounds/star.ogg', './src/assets/sounds/star.mp3']
-  });
+  }),
   click: new Howl({
     src: ['./src/assets/sounds/click.ogg', './src/assets/sounds/click.mp3']
-  });
+  }),
   correct: new Howl({
     src: ['./src/assets/sounds/correct.ogg', './src/assets/sounds/correct.mp3']
-  });
+  }),
   wrong: new Howl({
     src: ['./src/assets/sounds/wrong.ogg', './src/assets/sounds/wrong.mp3']
-  });
+  })
 }
